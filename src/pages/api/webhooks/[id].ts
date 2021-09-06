528406efb9ad9c0b346e13833899c03519eaeec4
Sarath <sarath.tk@cordiant.com>
2021-09-06 16:49:36 +0530

    boom :boom:

---
+++ src/pages/api/webhooks/[id].ts
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+
+    const { id } = req.query
+    res.status(200).json({ message: `${id} success ` })
+
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <div>
+      <h1>Webhook data console ⚓️ </h1>
+      <p>Made by Sarath ⚡️</p>
+    </div>
+  );
+}
